b108cf4ffbfa8c41cb98c7a6e31b5086f1874445
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2018-05-03 15:50:37 -0300

    Fix PEP8 and pylint warnings
    Signed-off-by: Lucas Dutra Nunes <ldnunes@ossystems.com.br>

--- examples/state_change_listener.py
+++ examples/state_change_listener.py
@@ -7,16 +7,21 @@ def callback(action, state, command):
     print("CALLBACK: " + action + " " + state)
     command.cancel()
 
+
 def error_callback(error_message, command):
     print("ERROR: " + error_message)
     command.try_again(10)
 
+
 def main():
     scl = updatehub.StateChangeListener()
-    scl.on(updatehub.Action.ENTER, updatehub.State.DOWNLOADING, callback)
+    scl.on_state_change(updatehub.Action.ENTER,
+                        updatehub.State.DOWNLOADING,
+                        callback)
     scl.on_error(error_callback)
 
     scl.start()
+
 
 if __name__ == '__main__':
     main()
--- updatehub/__init__.py
+++ updatehub/__init__.py
@@ -25,16 +25,16 @@ class Action(Enum):
 
 @unique
 class State(Enum):
-    IDLE        = "idle"
-    POLL        = "poll"
-    PROBE       = "probe"
+    IDLE = "idle"
+    POLL = "poll"
+    PROBE = "probe"
     DOWNLOADING = "downloading"
-    DOWNLOADED  = "downloaded"
-    INSTALLING  = "installing"
-    INSTALLED   = "installed"
-    EXIT        = "exit"
-    ERROR       = "error"
-    REBOOTING   = "rebooting"
+    DOWNLOADED = "downloaded"
+    INSTALLING = "installing"
+    INSTALLED = "installed"
+    EXIT = "exit"
+    ERROR = "error"
+    REBOOTING = "rebooting"
 
 
 class StateCommand:
@@ -52,14 +52,17 @@ class StateCommand:
         self._connection.send(message.encode())
 
 
-class MalformedState(Exception): pass
+class MalformedState(Exception):
+    pass
 
 
-class StateError(Exception): pass
+class StateError(Exception):
+    pass
 
 
 class StateChangeListener():
-    SDK_STATECHANGE_TRIGGER_FILENAME = "/usr/share/updatehub/state-change-callbacks.d/10-updatehub-sdk-statechange-trigger"
+    SDK_TRIGGER_FILENAME = ("/usr/share/updatehub/state-change-callbacks.d/"
+                            "10-updatehub-sdk-statechange-trigger")
     SOCKET_PATH = "/run/updatehub-statechange.sock"
     CANCEL_MESSAGE = "cancel"
     TRY_AGAIN_MESSAGE = "try_again"
@@ -69,8 +72,9 @@ class StateChangeListener():
         self.listeners = {}
         self.sock = None
         self.running = False
+        self.thread = threading.Thread(target=self._loop)
 
-    def on(self, action, state, callback):
+    def on_state_change(self, action, state, callback):
         key = action.value + "_" + state.value
         if self.listeners.get(key) is None:
             self.listeners[key] = []
@@ -80,28 +84,49 @@ class StateChangeListener():
         self.error_handlers.append(callback)
 
     def start(self):
-        if not os.path.isfile(StateChangeListener.SDK_STATECHANGE_TRIGGER_FILENAME):
+        if not os.path.isfile(StateChangeListener.SDK_TRIGGER_FILENAME):
             print("updatehub-sdk-statechange-trigger not found!")
             exit(1)
 
         self.running = True
-        self.thread = threading.Thread(target=self._loop)
         self.thread.start()
+
+    @classmethod
+    def _get_state(cls, line):
+        parts = line.split(' ')
+
+        if parts[0] == "error":
+            raise StateError(" ".join(parts[1:]))
+
+        if len(parts) < 2:
+            raise MalformedState()
+
+        return parts[0], parts[1]
+
+    @classmethod
+    def _readline(cls, conn):
+        buff = io.BytesIO()
+        while True:
+            data = conn.recv(16)
+            buff.write(data)
+            if b'\n' in data:
+                break
+        return buff.getvalue().splitlines()[0].decode("utf-8")
 
     def _loop(self):
         while self.running:
             try:
                 self._connect()
                 self._wait_for_state()
-            except OSError as e:
-                if self.running == False and e.errno == 9:
+            except OSError as exception:
+                if self.running is False and exception.errno == 9:
                     pass
                 else:
                     self.running = False
-                    raise e
-            except Exception as e:
+                    raise exception
+            except Exception as exception:
                 self.running = False
-                raise e
+                raise exception
             finally:
                 self.sock.close()
 
@@ -114,12 +139,12 @@ class StateChangeListener():
         while True:
             conn = None
             try:
-                conn, addr = self.sock.accept()
+                conn = self.sock.accept()[0]
                 line = self._readline(conn)
-                action, state = self._get_state(line, conn)
+                action, state = self._get_state(line)
                 self._emit(action, state, conn)
-            except StateError as e:
-                self._throw_error(e, conn)
+            except StateError as exception:
+                self._throw_error(exception, conn)
             finally:
                 if conn is not None:
                     conn.close()
@@ -132,20 +157,9 @@ class StateChangeListener():
         self.sock.bind(StateChangeListener.SOCKET_PATH)
         self.sock.listen(1)
 
-    def _get_state(self, line, connection):
-        parts = line.split(' ')
-
-        if parts[0] == "error":
-            raise StateError(" ".join(parts[1:]))
-
-        if len(parts) < 2:
-            raise MalformedState()
-
-        return parts[0], parts[1]
-
     def _emit(self, action, state, connection):
         key = action + "_" + state
-        for callback in (self.listeners.get(key) or []):
+        for callback in self.listeners.get(key) or []:
             command = StateCommand(connection)
             callback(action, state, command)
 
@@ -153,12 +167,3 @@ class StateChangeListener():
         for callback in self.error_handlers:
             command = StateCommand(connection)
             callback(str(exception), command)
-
-    def _readline(self, conn):
-        buff = io.BytesIO()
-        while True:
-            data = conn.recv(16)
-            buff.write(data)
-            if b'\n' in data:
-                break
-        return buff.getvalue().splitlines()[0].decode("utf-8")
